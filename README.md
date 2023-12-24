4cdc4dba05667b31f5e961c7750c6f3701bd20a9
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2023-12-24 11:02:44 +0300

    Add docker scripts and update README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ur_sim_docker
 Docker image of the ur robot simulation environment. 
+
+* Ubuntu 20
+* ROS `noetic` + Moveit!
+* Gazebo 11
+* Dev tools e.g. VSCode
+
+# Building Docker Image
+* Docker should be installed
+* Clone this package `git clone https://github.com/mzahana/ur_sim_docker.git`
+* Build docker image
+    ```bash
+    cd ur_sim_docker/docker
+    make ur_noetic_cuda11.7
+    ```
+
+# Run
+* Run the following script to enter the docker container `ur_sim`
+    ```bash
+    cd ur_sim_docker
+    ./run_ur_cuda_container.sh
+    ```
+# Clone Packages
+* You will need to clone some packages from source, such as the UR robots
+* Add these packages in the shared volume. The shared volume in the host machine is in `$HOME/ur_sim_shared_volume/`. And it is located inside the container in `/home/user/shared_volume`. This allows to share material easily between the container and the host machine. The `catkin_ws` is located inside the shared volume. ROS packages cloned from source should be inside the `catkin_ws/src` inside the shared volume.
---
+++ run_ur_cuda_container.sh
@@ -0,0 +1,140 @@
+#! /bin/bash
+## ROS noetic + Moveit! + Yolov8 + CUDA 11.7
+# Authors: Mohammed Abdelkader, mohamedashraf123@gmail.com
+
+
+DOCKER_REPO="mzahana/ur_noetic_cuda11.7"
+CONTAINER_NAME="ur_sim"
+WORKSPACE_DIR=~/${CONTAINER_NAME}_shared_volume
+CMD=""
+DOCKER_OPTS=""
+SUDO_PASSWORD="user"
+
+# Get the current version of docker-ce
+# Strip leading stuff before the version number so it can be compared
+DOCKER_VER=$(dpkg-query -f='${Version}' --show docker-ce | sed 's/[0-9]://')
+if dpkg --compare-versions 19.03 gt "$DOCKER_VER"
+then
+    echo "Docker version is less than 19.03, using nvidia-docker2 runtime"
+    if ! dpkg --list | grep nvidia-docker2
+    then
+        echo "Please either update docker-ce to a version greater than 19.03 or install nvidia-docker2"
+	exit 1
+    fi
+    DOCKER_OPTS="$DOCKER_OPTS --runtime=nvidia"
+else
+    DOCKER_OPTS="$DOCKER_OPTS --gpus all"
+fi
+echo "GPU arguments: $DOCKER_OPTS"
+
+# This will enable running containers with different names
+# It will create a local workspace and link it to the image's catkin_ws
+if [ "$1" != "" ]; then
+    CONTAINER_NAME=$1
+fi
+WORKSPACE_DIR=~/${CONTAINER_NAME}_shared_volume
+if [ ! -d $WORKSPACE_DIR ]; then
+    mkdir -p $WORKSPACE_DIR
+fi
+echo "Container name:$CONTAINER_NAME WORSPACE DIR:$WORKSPACE_DIR" 
+
+
+if [ "$2" != "" ]; then
+    CMD=$2
+fi
+
+XAUTH=/tmp/.docker.xauth
+xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+if [ ! -f $XAUTH ]
+then
+    echo XAUTH file does not exist. Creating one...
+    touch $XAUTH
+    chmod a+r $XAUTH
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    fi
+fi
+# Prevent executing "docker run" when xauth failed.
+if [ ! -f $XAUTH ]
+then
+  echo "[$XAUTH] was not properly created. Exiting..."
+  exit 1
+fi
+echo "Shared WORKSPACE_DIR: $WORKSPACE_DIR";
+
+#not-recommended - check this: http://wiki.ros.org/docker/Tutorials/GUI
+xhost +local:root
+ 
+echo "Starting Container: ${CONTAINER_NAME} with REPO: $DOCKER_REPO"
+
+CMD="export DEV_DIR=/home/user/shared_volume && \
+    export CATKIN_WS=\$DEV_DIR/catkin_ws &&\
+        source /home/user/.bashrc &&\
+        if [ -f "/home/user/shared_volume/catkin_ws/devel/setup.bash" ]; then
+            source /home/user/shared_volume/catkin_ws/devel/setup.bash
+        fi &&\
+         /bin/bash"
+if [[ -n "$GIT_TOKEN" ]] && [[ -n "$GIT_USER" ]]; then
+    CMD="export GIT_USER=$GIT_USER && export GIT_TOKEN=$GIT_TOKEN && $CMD"
+fi
+
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    CMD="export SUDO_PASSWORD=$SUDO_PASSWORD && $CMD"
+fi
+
+#
+# Run/Restart container
+#
+if [ "$(docker ps -aq -f name=${CONTAINER_NAME})" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=${CONTAINER_NAME})" ]; then
+        # cleanup
+        echo "Restarting the container..."
+        docker start ${CONTAINER_NAME}
+    fi
+
+    docker exec --user user -it ${CONTAINER_NAME} env TERM=xterm-256color bash -c "${CMD}"
+
+else
+
+    CMD="export DEV_DIR=/home/user/shared_volume &&\
+        export CATKIN_WS=\$DEV_DIR/catkin_ws &&\
+        source /home/user/.bashrc &&\
+        if [ -f "/home/user/shared_volume/catkin_ws/devel/setup.bash" ]; then
+            source /home/user/shared_volume/catkin_ws/devel/setup.bash
+        fi &&\
+        /bin/bash"
+
+    if [[ -n "$GIT_TOKEN" ]] && [[ -n "$GIT_USER" ]]; then
+    CMD="export GIT_USER=$GIT_USER && export GIT_TOKEN=$GIT_TOKEN && $CMD"
+    fi
+
+    if [[ -n "$SUDO_PASSWORD" ]]; then
+        CMD="export SUDO_PASSWORD=$SUDO_PASSWORD && $CMD"
+    fi
+
+    echo "Running container ${CONTAINER_NAME}..."
+
+    #-v /dev/video0:/dev/video0 \
+    # --publish 14556:14556/udp \
+    # --env="QT_X11_NO_MITSHM=1" \
+    # --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    # --volume="$XAUTH:$XAUTH" \
+    # -env="XAUTHORITY=$XAUTH" \
+    
+    docker run -it \
+        --network host \
+        --env="DISPLAY=${DISPLAY}" \
+        -e NVIDIA_DRIVER_CAPABILITIES=all \
+        -e LOCAL_USER_ID="$(id -u)" \
+        --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
+        --volume="/etc/localtime:/etc/localtime:ro" \
+        --volume="$WORKSPACE_DIR:/home/user/shared_volume:rw" \
+        --volume="/dev:/dev" \
+        --name=${CONTAINER_NAME} \
+        --privileged \
+        --workdir /home/user/shared_volume \
+        $DOCKER_OPTS \
+        ${DOCKER_REPO} \
+        bash -c "${CMD}"
+fi
